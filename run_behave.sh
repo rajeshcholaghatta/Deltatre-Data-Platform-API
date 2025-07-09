e04aa2bb90a657c2becce42bf38637152c59dd5a
rajeshcholaghatta <rajesh.cholaghatta@deltatre.com>
2025-07-09 19:28:17 +0530

    Delete run_behave.sh

--- run_behave.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-rm -rf allure-results
-mkdir -p allure-results
-behave --no-capture -t '@smoke, @regression' -f allure_behave.formatter:AllureFormatter -o allure-results
