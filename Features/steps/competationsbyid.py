5d675ff43ebb422c912eeab435d1c75e26c7dab4
rajeshcholaghatta <rajesh.cholaghatta@deltatre.com>
2025-07-11 10:30:33 +0530

    Update competationsbyid.py

--- Features/steps/competationsbyid.py
+++ Features/steps/competationsbyid.py
@@ -7,6 +7,7 @@ from behave import *
 import requests
 from jsonschema.exceptions import ValidationError
 from jsonschema.validators import validate
+from utils.logger import get_logger
 
 @given('the competations details to be fetched based on ID from deltatre sports platform with parameter {locale} variable')
 def api(context,locale):
